cab4079882b6e088e75523012efc4c8edf6a7715
Ryan McKern <ryan.mckern@puppetlabs.com>
2016-11-05 00:57:19 -0700

    Update Jdupes to 1.5.1

--- jdupes.rb
+++ jdupes.rb
@@ -1,8 +1,8 @@
 class Jdupes < Formula
   desc "Powerful duplicate file finder and an enhanced fork of 'fdupes'."
   homepage "https://github.com/jbruchon/jdupes/"
-  url "https://github.com/jbruchon/jdupes/archive/v1.5.tar.gz"
-  sha256 "0672f424c5f0f7fb6daecb57565a502d55118ea30bee1a4cb839cbce12e20f86"
+  url "https://github.com/jbruchon/jdupes/archive/v1.5.1.tar.gz"
+  sha256 "5159a79f7c2ff814def9759b993040271023ea095f98a0cb3374d53555df127d"
 
   head "https://github.com/jbruchon/jdupes.git"
 
