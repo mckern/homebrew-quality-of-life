2a2fe4a6e876aaaf5c9181c18c2f06529a3e05fe
Ryan McKern <ryan@orangefort.com>
2022-02-05 22:47:38 -0800

    2022 updates

--- Formula/inverted_commas.rb
+++ Formula/inverted_commas.rb
@@ -1,30 +1,30 @@
 # frozen_string_literal: true
 
 class InvertedCommas < Formula
-  desc 'Plaintext filter to strip smart quotes'
-  homepage 'https://github.com/mckern/inverted-commas'
+  desc "Plaintext filter to strip smart quotes"
+  homepage "https://github.com/mckern/inverted-commas"
 
-  url 'https://github.com/mckern/inverted-commas.git',
-      revision: 'v0.0.3'
-  version '0.0.3'
+  url "https://github.com/mckern/inverted-commas.git",
+      revision: "v0.0.3"
+  version "0.0.3"
 
-  depends_on 'dep' => :build
-  depends_on 'go' => :build
+  depends_on "dep" => :build
+  depends_on "go" => :build
 
   def install
-    ENV['GOPATH'] = buildpath
-    dir = buildpath / 'src/github.com/mckern/inverted-commas'
+    ENV["GOPATH"] = buildpath
+    dir = buildpath / "src/github.com/mckern/inverted-commas"
     dir.install buildpath.children
 
     cd dir do
-      system 'dep', 'ensure', '-vendor-only'
-      system 'go', 'build', '-o', 'ic'
-      bin.install 'ic'
+      system "dep", "ensure", "-vendor-only"
+      system "go", "build", "-o", "ic"
+      bin.install "ic"
     end
   end
 
   test do
     assert_equal %q("This was some fancy text", wasn't it?),
-                 pipe_output("#{bin}/ic", '‟This was some fancy text”, wasn’t it?', 0).chomp
+                 pipe_output("#{bin}/ic", "‟This was some fancy text”, wasn’t it?", 0).chomp
   end
 end
--- Formula/symlinks.rb
+++ Formula/symlinks.rb
@@ -1,23 +1,23 @@
 # frozen_string_literal: true
 
 class Symlinks < Formula
-  desc 'Fix, alter, manage symlinks'
-  homepage 'https://github.com/cometsong/symlinks/'
+  desc "Fix, alter, manage symlinks"
+  homepage "https://github.com/cometsong/symlinks/"
 
-  url 'https://github.com/cometsong/symlinks.git',
-      revision: '4c01a15f8cc3de9bcb31e0cbabcbfb6af33b4aae'
-  version '4c01a15f'
+  url "https://github.com/cometsong/symlinks.git",
+      revision: "4c01a15f8cc3de9bcb31e0cbabcbfb6af33b4aae"
+  version "4c01a15f"
 
   def install
-    system 'make', "PREFIX=#{prefix}"
-    bin.install 'symlinks'
-    man8.install 'symlinks.8'
+    system "make", "PREFIX=#{prefix}"
+    bin.install "symlinks"
+    man8.install "symlinks.8"
   end
 
   test do
-    ln_s 'a', 'b'
+    ln_s "a", "b"
 
-    assert_match 'b -> a (reason: No such file or directory)',
+    assert_match "b -> a (reason: No such file or directory)",
                  shell_output("#{bin}/symlinks -d .")
   end
 end
